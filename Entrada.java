99dff243b4d3e4575eabebef6d219a804bb9faf9
gunTelles16 <825151851@Ulife.com.br>
2025-03-14 10:59:50 -0300

    Trabalhando com entradas de dados

---
+++ Entrada.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Entrada {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome:");
+        String nome = scanner.nextLine();
+
+        System.err.println("Digite a sua idade:");
+        int idade = scanner.nextInt();
+
+        System.err.println("Olá " + nome + ", você tem " + idade + " anos, seja bem-vindo (a)");
+    }    
+}
